6e80b5ef434e4f633bd52729d41abc506c012168
Amit Philips <amitphilips@live.com>
2022-01-27 14:25:11 +0530

    BST add and find implementation

---
+++ DS/3-Trees/src/BinarySearchTreeApp/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.WriteLine("-----------Binary Search Tree------------");
+
+REPL.Run();
+
+public class BST<T>
+{
+    public T Data { get; set; }
+    public BST<T> Left { get; set; }
+    public BST<T> Right { get; set; }
+
+    public BST(T value)
+    {
+        Data = value;
+    }
+
+    public void Add(T value)
+    {
+        if (Comparer<T>.Default.Compare(value, Data) < 0)
+        {
+            if (Left != null)
+            {
+                Left.Add(value);
+            }
+            else
+            {
+                Left = new BST<T>(value);
+            }
+        }
+        else
+        {
+            if (Right != null)
+            {
+                Right.Add(value);
+            }
+            else
+            {
+                Right = new BST<T>(value);
+            }
+        }
+    }
+
+    public void Find(T value)
+    {
+        if(Data != null)
+        {
+            if(Comparer<T>.Default.Compare(value,Data) < 0)
+            {
+                if(Left != null)
+                {
+                    Left.Find(value);
+                }
+                else
+                {
+                    Console.WriteLine($"{value} Not Found");
+                }
+            }
+            else if(Comparer<T>.Default.Compare(value, Data) > 0)
+            {
+                if (Right != null)
+                {
+                    Right.Find(value);
+                }
+                else
+                {
+                    Console.WriteLine($"{value} Not Found");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{value} exists in BST");
+            }
+        }
+    }
+
+    public void ListAll()
+    {
+
+    }
+}
+
+
---
+++ DS/3-Trees/src/BinarySearchTreeApp/REPL.cs
@@ -0,0 +1,53 @@
+﻿public static class REPL
+{
+    public static void Run()
+    {
+        var n1 = new BST<int>(5);
+        n1.Add(3);
+        n1.Add(7);  
+        n1.Add(2);
+        n1.Add(4);
+        n1.Add(1);
+        n1.Add(6);
+        n1.Add(8);
+
+        string? input = "";
+
+        while (input != "exit")
+        {
+            Console.WriteLine("Operations : add | find | list | remove | exit");
+            input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "add":
+                    {
+                        var num = Console.ReadLine();
+                        n1.Add(Convert.ToInt32(num));
+                        Console.WriteLine("Pushed");
+                        break;
+                    }
+                case "find":
+                    {
+                        var num = Console.ReadLine();
+                        n1.Find(Convert.ToInt32(num));
+                        break;
+                    }
+                case "list":
+                    {
+                        //n1.ListAll();
+                        break;
+                    }
+                case "remove":
+                    {
+                        //n1.Remove();
+                        Console.WriteLine("Pop");
+                        break;
+                    }
+                case "exit": input = "exit"; break;
+                default: break;
+
+            }
+        }
+    }
+}
