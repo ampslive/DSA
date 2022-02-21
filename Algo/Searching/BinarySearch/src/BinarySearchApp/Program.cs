1913856071a74d1d925f32af97692f411e5778cc
Amit Philips <amitphilips@live.com>
2022-02-21 13:28:37 +0530

    Binary Search implementation

--- Algo/Searching/BinarySearch/src/BinarySearchApp/Program.cs
+++ Algo/Searching/BinarySearch/src/BinarySearchApp/Program.cs
@@ -2,7 +2,7 @@
 var input = new int[] {1, 2, 3, 4, 5};
 Console.WriteLine($"Input Array: {String.Join(", ", input)}");
 
-int target = 4;
+int target = 7;
 Console.WriteLine($"Search term: {target}");
 
 int left = 0;
@@ -10,6 +10,9 @@ int right = input.Length - 1;
 
 var res = BinarySearch(input, left, right, target);
 Console.WriteLine($"Search Result Index {res}");
+
+int res2 = BinarySearchRecursive(input, left, right, target);
+Console.WriteLine($"Recursive: Search Result Index {res2}");
 Console.ReadLine();
 
 static int BinarySearch(int[] input, int left, int right, int target)
@@ -35,6 +38,30 @@ static int BinarySearch(int[] input, int left, int right, int target)
         }
     }
     return targetIndex;
+}
+
+static int BinarySearchRecursive(int[] input, int left, int right, int target)
+{
+    if (left > right)
+        return -1;
+
+    int mid = (left + right)/2;
+    if (input[mid] == target)
+    {
+        return mid;
+    }
+    else if(target < input[mid])
+    {
+        left = 0;
+        right = mid - 1;
+        return BinarySearchRecursive(input, left, right, target);
+    }
+    else
+    {
+        left = mid + 1;
+        right = input.Length - 1;
+        return BinarySearchRecursive(input, left, right, target);
+    }
 }
 
 
