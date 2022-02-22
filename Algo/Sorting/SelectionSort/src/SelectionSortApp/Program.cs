ed9df824cad0a1a31c59830e1562dc7170409e67
Amit Philips <amitphilips@live.com>
2022-02-22 13:22:38 +0530

    Selection sort implementation

--- Algo/Sorting/SelectionSort/src/SelectionSortApp/Program.cs
+++ Algo/Sorting/SelectionSort/src/SelectionSortApp/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Selection Sort!");
+
+var inputArray = new int[] { 5, 3, 8, 1, 6, 2 };
+
+Console.WriteLine($"Input Array: {String.Join(", ", inputArray)}");
+Console.WriteLine($"Output Array: {String.Join(", ", SelectionSort(inputArray))}");
+Console.ReadLine();
+
+static int[] SelectionSort(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        int min = i;
+
+        for(int j=i+1; j < arr.Length; j++)
+        {
+            if(arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        if(min != i)
+        {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    return arr;
+}
