3445df4006bb392f041885d6a0f89c0dfe7b71c6
Amit Philips <amitphilips@live.com>
2022-02-28 14:08:07 +0530

    BuySellStock Algo

---
+++ Algo/BuySellStock/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Buy and Sell Stocks!");
+
+int[] inputArray = {2,4,3,3,1,7};
+Console.WriteLine($"Stock Prices (day-wise): {String.Join(", ", inputArray)}");
+
+Console.WriteLine($"Max Profit: {CalculateProfit(inputArray)}");
+
+/// <summary>
+/// For each day wise price, add to an existing profit variable when there is difference in price between currentDay and currentDay-1
+/// </summary>
+static int CalculateProfit(int[] arr)
+{
+    int profit = 0;
+
+    for(int i=1; i<arr.Length; i++)
+    {
+        if(arr[i] > arr[i-1])
+        {
+            profit += (arr[i] - arr[i-1]);
+        }
+    }
+
+    return profit;
+}
+
