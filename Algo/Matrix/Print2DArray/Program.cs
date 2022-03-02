ea22aafa73eeec8e5c1502295c4998d0d43d4409
Amit Philips <amitphilips@live.com>
2022-03-02 15:01:15 +0530

    Print 2D Matrix

---
+++ Algo/Matrix/Print2DArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Print 2D Array!");
+
+int[,] inputArray = {{1,2,3,4},{5,6,7,8}};
+
+Console.WriteLine("Output of matrix:");
+
+PrintMatrix(inputArray);
+Console.ReadLine();
+
+static void PrintMatrix(int[,] arr)
+{
+    int height = arr.GetLength(0);
+    int width = arr.GetLength(1);
+    
+    for(int i=0; i < height*width; i++)
+    {
+        int row = i/width;
+        int col = i%width;
+        Console.WriteLine(arr[row,col]);
+    }
+}
+
